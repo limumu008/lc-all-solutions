4b88f97a709d22abcb6b4dc487b214eaff1f4e76
Nan Li <limumu008@gmail.com>
2019-04-06 19:08:04 -0500

    Update palindrome-permutation.py

--- 266.palindrome-permutation/palindrome-permutation.py
+++ 266.palindrome-permutation/palindrome-permutation.py
@@ -7,18 +7,20 @@ class Solution(object):
     evenCount = 0
     oddCount = 0
     d = {}
+    # Put each charcters into a Dictionary such as {'a':2,'b':1}
     for c in s:
-      d[c] = d.get(c, 0) + 1
+        d[c] = d.get(c,0)+1
+    # Count odd charcters and even charcters.
     for k in d:
-      if d[k] % 2 == 1:
-        oddCount += 1
-      else:
-        evenCount += 1
-
+        if d[k] % 2 == 1:
+            oddCount += 1
+        else:
+            evenCount += 1
+    # Only if oddCount == 1 or oddCount == 0 return True
     if len(s) % 2 == 1:
-      if oddCount == 1:
-        return True
+        if oddCount == 1:
+            return True
     else:
-      if oddCount == 0:
-        return True
+        if oddCount == 0:
+            return True
     return False
